ab340eabdc6b1ae311ee6b0873c97e8ed36b3b69
unknown <shamistan2002@icloud.com>
2022-05-18 00:11:24 +0400

    нахождение максимального элемента в векторе

---
+++ Работа с библиотекой STL (Вектор, дек, список)/task_1_4.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+	setlocale(LC_ALL, "RUS");
+
+	vector<int> vec = { 10,2,3,10,4,5,10,5,6,10,1,6,7,10 }; // создание и заполнение вектора 
+
+	for (vector<int>::iterator i = vec.begin(); i != vec.end(); i++) {
+
+		cout << *i << " ";
+
+	}
+
+	cout << endl;
+
+	int Max = *max_element(vec.begin(), vec.end()); // для нахождения максимального элемента в векторе 
+
+	int k = count(vec.begin(), vec.end(), Max);   // для нахождения количества максимальных элементов в векторе 
+
+
+	cout << "Максимальный элемент - " << Max << endl;
+
+	cout << "Количество максимальных элементов - " << k << endl;
+
+	cin.get();
+	cin.get();
+	return 0;
+}
